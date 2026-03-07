7823eaf1a72361a75084152f83025a971dae109f
ygmsoy-web <y.gmsoy@gmail.com>
2026-03-07 14:56:44 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'sinif-defteri-v1';
+const ASSETS = [
+  '/sinif-defteri/sinif_defteri.html',
+  'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS).catch(()=>{}))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('/sinif-defteri/sinif_defteri.html')))
+  );
+});
